88dc670031461286be7370e7b4fcdf89501b5fc5
adailsonacj <adailsonacj@live.com>
2019-04-30 17:08:24 -0300

    No lugar de Hash do arquivo guardando date (long)

--- src/atualizeme/test/ArquivoMD5.java
+++ src/atualizeme/test/ArquivoMD5.java
@@ -121,28 +121,8 @@ public class ArquivoMD5 {
 	}
 
 	public String geraHash(File f) throws NoSuchAlgorithmException, FileNotFoundException {
-		MessageDigest digest = MessageDigest.getInstance("MD5");
-		InputStream is = new FileInputStream(f);
-		byte[] buffer = new byte[8192];
-		int read = 0;
-		String output = null;
-		try {
-			while ((read = is.read(buffer)) > 0) {
-				digest.update(buffer, 0, read);
-			}
-			byte[] md5sum = digest.digest();
-			BigInteger bigInt = new BigInteger(1, md5sum);
-			output = bigInt.toString(16);
-		} catch (IOException e) {
-			throw new RuntimeException("Não foi possivel processar o arquivo.", e);
-		} finally {
-			try {
-				is.close();
-			} catch (IOException e) {
-				throw new RuntimeException("Não foi possivel fechar o arquivo", e);
-			}
-		}
-		return output;
+		String date = "" + f.lastModified();
+		return date;
 	}
 
 	public List<ArquivoTxt> arquivosExcluir(List<ArquivoTxt> listaServidor, List<ArquivoTxt> listacliente) {
